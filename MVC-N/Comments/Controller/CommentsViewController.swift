cb82e3dee9704413e31e3eebc1c1c20d70a9f1e5
id2k1149 <id2k1149@gmail.com>
2023-08-11 13:42:04 -0400

    add @IBOutlet weak var tableView: UITableView! to CommentsViewController.swift

--- MVC-N/Comments/Controller/CommentsViewController.swift
+++ MVC-N/Comments/Controller/CommentsViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class CommentsViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
